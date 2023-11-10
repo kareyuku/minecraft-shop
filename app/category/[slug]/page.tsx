4ddd03582a5b1747e47deaace70fab6791a4674b
b4rtuus <bartixfajny@gmail.com>
2023-11-10 22:09:17 +0100

    Server Fetching

--- app/category/[slug]/page.tsx
+++ app/category/[slug]/page.tsx
@@ -1,16 +1,40 @@
 import Navbar from "@/components/Navbar";
-import ProductsGrid from "@/components/Shop/ProductsGrid";
+import RecentPurchases from "@/components/Shop/Modules/RecentPurchases";
+import ProductCard from "@/components/Shop/ProductCard";
 import { prisma } from "@/lib/prisma";
+import { cache } from "react";
+
+export const getProducts = cache(async (serverId: number) => {
+  const products = await prisma.product.findMany({ where: { serverId } });
+  const payments = await prisma.paymentMethod.findMany({
+    distinct: ["currency", "fee", "id", "provider"],
+  });
+  return { products, payments };
+});
 
 export default async function CategoryPage({
   params,
 }: {
   params: { slug: string };
 }) {
+  const { products, payments } = await getProducts(parseInt(params.slug));
   return (
     <main className="container mx-auto flex flex-col gap-6 mt-10 px-3 max-sm:px-5">
       <Navbar />
-      <ProductsGrid serverId={params.slug} />
+      <section className="flex max-sm:flex-col gap-10">
+        <section className="flex-[0.7] grid max-sm:grid-cols-2 max-md:grid-cols-2 max-lg:grid-cols-3 grid-cols-4 gap-4">
+          {products.map((product) => (
+            <ProductCard
+              product={product}
+              paymentsMethods={payments}
+              key={product.id}
+            />
+          ))}
+        </section>
+        <section className="flex-[0.3]">
+          <RecentPurchases />
+        </section>
+      </section>
     </main>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -2,20 +2,28 @@ import Navbar from "@/components/Navbar";
 import CategoryCard from "@/components/Shop/CategoryCard";
 import RecentPurchases from "@/components/Shop/Modules/RecentPurchases";
 import { prisma } from "@/lib/prisma";
+import { cache } from "react";
+
+export const revalidate = 3600;
+
+export const getServers = cache(async () => {
+  const servers = await prisma.server.findMany();
+  return servers;
+});
 
 export default async function LandingPage() {
-  const Servers = await prisma.server.findMany();
+  const servers = await getServers();
   return (
     <main className="container mx-auto flex flex-col gap-6 mt-10 px-3 max-sm:px-5">
       <Navbar />
       <section className="flex max-sm:flex-col gap-10">
         <section className="flex-[0.7] grid max-sm:grid-cols-1 max-md:grid-cols-2 max-lg:grid-cols-3 grid-cols-4 gap-4">
-          {Servers.map((Server) => (
+          {servers.map((server) => (
             <CategoryCard
-              name={Server.name}
-              img={Server.imageUri || ""}
-              serverId={Server.id.toString()}
-              key={Server.id}
+              name={server.name}
+              img={server.imageUri || ""}
+              serverId={server.id.toString()}
+              key={server.id}
             />
           ))}
         </section>
