883b411462730c29326216b50a51fbfc5f24f338
b4rtuus <bartixfajny@gmail.com>
2023-11-11 23:01:51 +0100

    cleaning up

---
+++ components/dashboard/ServersGrid/hooks/useServers.tsx
@@ -0,0 +1,31 @@
+"use client";
+
+import { Server } from "@prisma/client";
+import { useEffect, useState } from "react";
+
+export default function useServers() {
+  const [servers, setServers] = useState<Server[]>([]);
+
+  const fetchServers = async () => {
+    const response = await fetch("/api/servers");
+    setServers(await response.json());
+  };
+
+  const addServer = (server: Server) => setServers([...servers, server]);
+
+  const removeServer = (serverId: number) =>
+    setServers(servers.filter((server: Server) => server.id !== serverId));
+
+  const editServer = async (server: Server) => {
+    const oldServers = [...servers];
+    const index = oldServers.findIndex((serv) => serv.id == server.id);
+    oldServers[index] = server;
+    setServers([...oldServers]);
+  };
+
+  useEffect(() => {
+    fetchServers();
+  }, []);
+
+  return { servers, addServer, removeServer, editServer };
+}
--- components/dashboard/ServersGrid/index.tsx
+++ components/dashboard/ServersGrid/index.tsx
@@ -1,37 +1,11 @@
 "use client";
 
 import ServerCard from "./ServerCard";
-import { useEffect, useState } from "react";
-import { Server } from "@prisma/client";
 import CreateServerModal from "@/components/Modals/CreateServerModal";
+import useServers from "./hooks/useServers";
 
 export default function ServersGrid() {
-  const [servers, setServers] = useState<Server[]>([]);
-  const [loading, setLoading] = useState(true);
-
-  const loadServers = async () => {
-    const data = await fetch("/api/servers");
-    setServers(await data.json());
-    setLoading(false);
-  };
-
-  const addServer = (server: Server) => setServers([...servers, server]);
-
-  const removeServer = (serverId: number) =>
-    setServers(servers.filter((server: Server) => server.id !== serverId));
-
-  const editServer = async (server: Server) => {
-    const oldServers = [...servers];
-    const index = oldServers.findIndex((serv) => serv.id == server.id);
-    oldServers[index] = server;
-    setServers([...oldServers]);
-  };
-
-  useEffect(() => {
-    loadServers();
-  }, []);
-
-  if (loading) return "Loading";
+  const { servers, addServer, editServer, removeServer } = useServers();
 
   return (
     <>
