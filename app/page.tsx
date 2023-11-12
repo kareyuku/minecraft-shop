e9ca738e1a6b3b3ff221d78048092260fcabdb4b
b4rtuus <bartixfajny@gmail.com>
2023-11-12 20:10:35 +0100

    Bigger Text

--- app/page.tsx
+++ app/page.tsx
@@ -17,6 +17,7 @@ export default async function LandingPage() {
     <main className="container mx-auto flex flex-col gap-6 mt-10 px-3 max-sm:px-5">
       <Navbar />
       <section className="flex flex-col gap-10">
+        <h1 className="text-2xl">Choose a Category</h1>
         <section className="grid max-sm:grid-cols-1 max-md:grid-cols-2 max-lg:grid-cols-3 grid-cols-3 gap-4">
           {servers.map((server) => (
             <CategoryCard
