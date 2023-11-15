0c09c0bdd554b0314bc20ddb63ca2f38b35ae003
b4rtuus <bartixfajny@gmail.com>
2023-11-15 01:55:37 +0100

    Added Transaction Model to prisma.schema

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,16 @@
 import { Suspense } from "react";
+import { BiSolidCategory } from "react-icons/bi";
+
 import Layout from "@/components/Shop/Layout";
 import CategoryGrid from "@/components/Shop/CategoryGrid";
 
 export default async function LandingPage() {
   return (
     <Layout>
-      <h1 className="text-3xl">Choose a Category</h1>
+      <div className="flex items-center gap-2 text-3xl text-third">
+        <BiSolidCategory />
+        <h1 className="text-white">Choose a Category</h1>
+      </div>
       <Suspense>
         <CategoryGrid />
       </Suspense>
